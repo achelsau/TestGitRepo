c6d485a6bd3635f12ec542de342aea23501e1c5c
Ariel Chelsau <ariel.chelsau@endava.com>
2013-09-12 17:26:34 +0300

    commit message

--- TestGitProject/src/com/endava/gitproject/GitTest.java
+++ TestGitProject/src/com/endava/gitproject/GitTest.java
@@ -9,7 +9,7 @@ package com.endava.gitproject;
 public class GitTest {
 
 	public static void main(String[] args) {
-		System.out.println("Git is fun");
+		System.out.println("Git is more and more fun");
 	}
 
 }
