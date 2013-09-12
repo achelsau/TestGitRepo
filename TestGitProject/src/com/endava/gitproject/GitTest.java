7c98ec2560ecb79395143a1e7e11d9b1e8618536
Ariel Chelsau <ariel.chelsau@endava.com>
2013-09-12 16:56:37 +0300

    abc

---
+++ TestGitProject/src/com/endava/gitproject/GitTest.java
@@ -0,0 +1,15 @@
+package com.endava.gitproject;
+
+/**
+ * Git Test main class
+ * 
+ * @author achelsau
+ *
+ */
+public class GitTest {
+
+	public static void main(String[] args) {
+		System.out.println("Git is fun");
+	}
+
+}
